95f4773b510a9b27cf4fd701280ea9fed264356c
sunny <sunnya2023@gmail.com>
2023-10-06 13:40:40 +0900

    first

---
+++ src/Components/LoginPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <>
+    <h1>로그인</h1>
+    <section>
+        <h2>이메일, 비밀번호 입력하는 곳</h2>
+        <form>
+            <input type='text' placeholder='이메일입력'/>
+            <input type='text' placeholder='비밀번호입력'/>
+            <button>로그인</button>
+        </form>
+    </section>
+    
+    </>
+  )
+}
